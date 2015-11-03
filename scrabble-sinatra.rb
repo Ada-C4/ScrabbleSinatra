5e723e852bb0b755958957d85d9a6a9168df6e79
emgord <emgord@gmail.com>
2015-11-03 14:50:19 -0800

    added a layout page

--- scrabble-sinatra.rb
+++ scrabble-sinatra.rb
@@ -4,24 +4,29 @@ require "./lib/scrabble_master"
 class ScrabbleSite < Sinatra::Base
 
   get "/" do
+    @header = "Scrabble Score"
     erb :index
   end
 
   get "/score" do
+    @header = "Score A Word"
     erb :score
   end
 
   post "/score" do
+    @header = "Score A Word"
     @word = params[:word]
     @score = Scrabble::Scrabble.score(@word)
     erb :score
   end
 
   get "/scoremore" do
+    @header = "Score Multiple Words"
     erb :scoremore
   end
 
   post "/scoremore" do
+    @header = "Score Multiple Words"
     all_words = params[:words]
     word_array = all_words.split(/\W+/)
     @word_hash = {}
