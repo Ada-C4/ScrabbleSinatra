e51bf72f361ea18a3824b071cf696f29d0f14ec4
Meighan Rasley <meighanr@gmail.com>
2015-11-03 13:35:06 -0800

    Add primary requirements

--- scrabble_site.rb
+++ scrabble_site.rb
@@ -1,4 +1,8 @@
 require "sinatra"
+require "./lib/player"
+require "./lib/scrabble"
+require "./lib/TileBag"
+require "./lib/dictionary"
 
 class ScrabbleSite < Sinatra::Base
 
@@ -9,4 +13,14 @@ class ScrabbleSite < Sinatra::Base
   get "/score" do
     erb :score
   end
+
+  post "/score" do
+    @words_scored = true
+    @words_played = params[:words]
+    @word_array = @words_played.split(" ")
+    @word_scores_hash = Scrabble::Scrabble.create_word_score_hash(@word_array)
+    erb :score
+  end
+
+
 end
