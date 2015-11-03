325e9d063bf4fa849ab9094677870fa8fbf7e6c3
Desiree <desireepoland@gmail.com>
2015-11-03 12:19:42 -0800

    Add link back to home page from score page, add form for word input, Add method to score input word, add code to display the score

--- scrabble-site.rb
+++ scrabble-site.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require './lib/scrabble'
 
 class ScrabbleSite < Sinatra::Base
   get "/" do
@@ -6,6 +7,13 @@ class ScrabbleSite < Sinatra::Base
   end
 
   get "/score" do
+    erb :score
+  end
+
+  post "/score" do
+    @word_scored = true
+    @word = params[:word]
+    @word_score =  Scrabble::Scrabble.score(@word)
     erb :score
   end
 
