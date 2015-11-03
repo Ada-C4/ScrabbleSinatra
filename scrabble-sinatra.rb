3276d3051266dcd7a885e553f99825f3975b38c0
Dreedle <davis.audrey.lynn@gmail.com>
2015-11-03 11:48:55 -0800

    Adding layout

--- scrabble-sinatra.rb
+++ scrabble-sinatra.rb
@@ -1,5 +1,8 @@
 require "sinatra"
-
+require "./lib/highest_score.rb"
+require "./lib/player.rb"
+require "./lib/score.rb"
+require "./lib/tilebag.rb"
 
 class ScrabbleSinatra < Sinatra::Base
 
@@ -8,7 +11,14 @@ class ScrabbleSinatra < Sinatra::Base
   end
 
   get "/score" do
+    @page_name = "Score a Word"
     erb :score
   end
+
+  get "/score_multiple" do
+    @page_name = "Score some Words"
+    erb :score_multiple
+  end
+
 
 end
