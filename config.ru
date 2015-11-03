20e1e838e39f64ea18288f01a4d4ea2c63b9bda8
emgord <emgord@gmail.com>
2015-11-02 16:31:40 -0800

    baseline requirements complete

---
+++ config.ru
@@ -0,0 +1,2 @@
+require './scrabble-sinatra'
+run ScrabbleSite
---
+++ scrabble-sinatra.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+
+class ScrabbleSite < Sinatra::Base
+
+  get "/" do
+    erb :index
+  end
+
+  get "/score" do
+    erb :score
+  end
+
+end
