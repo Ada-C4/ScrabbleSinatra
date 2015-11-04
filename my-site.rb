bca9f5365d11473d08dce7b1c4cd20d546cc533e
Annalee <annaleeherrera@gmail.com>
2015-11-03 17:16:19 -0800

    primary requirements

--- my-site.rb
+++ my-site.rb
@@ -1,4 +1,5 @@
 require "sinatra"
+require "./lib/scrabble/scrabble.rb"
 
 class MySite < Sinatra::Base
 
@@ -6,7 +7,31 @@ class MySite < Sinatra::Base
     erb :index
   end
 
+  get "/index.html" do
+    erb :index
+
+  end
   get "/score.html" do
     erb :score
+  end
+
+  get "/scoremultiple.html" do
+    erb :scoremultiple
+  end
+
+  post "/score.html" do
+    @word = params[:word]
+    @score=Scrabble::Scrabble.score(@word)
+    erb :score
+  end
+
+  post "/scoremultiple.html" do
+    words = params[:multiple_words1]
+    @words_array = words.split(", ")
+    @words_hash = {}
+    @words_array.each do |words|
+      @words_hash[words]= Scrabble::Scrabble.score(words)
+    end
+    erb :scoreview
   end
 end
